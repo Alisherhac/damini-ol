5a624bedbd0c46d5195af7f8e84e40dbcbc44112
hackeralone <isaqova565@gmail.com>
2022-03-25 09:35:57 +0300

    virus hac

---
+++ main.js
@@ -0,0 +1,24 @@
+// let son =prompt('sonni kiritin');
+// let natija=""
+
+// if(son%100==0){
+//     natija+=''
+// }
+
+// if (son%3==0){
+//     natija+='3,';
+// }
+// if (son%4==0){
+//     natija+='4,';
+
+// }
+// if (son%5==0){
+//     natija+='5,';
+// }
+// if (son%7==0){
+//     natija+='7,';
+// }
+// alert(natija || son)
+for(let i=2; i>1; i++){
+    console.log('xozir uxledi')
+}
